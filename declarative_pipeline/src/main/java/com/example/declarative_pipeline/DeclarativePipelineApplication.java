97d5683d4af70fcbed5b790d3e383b9adbf8eb64
HemantSakhare <sakhare.hemant@gmail.com>
2019-05-12 13:42:16 +0530

    first commit

---
+++ declarative_pipeline/src/main/java/com/example/declarative_pipeline/DeclarativePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.declarative_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarativePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarativePipelineApplication.class, args);
+	}
+
+}
